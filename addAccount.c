5578e5198de3ff63c0c7c58b6f0f67254279b3c4
HP <ebru@ubuntu.(none)>
2012-11-27 19:05:10 +0200

    i added a button that can be used when network is working

---
+++ addAccount.c
@@ -0,0 +1,43 @@
+#include <gtk/gtk.h>
+#include <gio/gio.h>
+
+int
+main (int   argc,
+char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *frame;
+  GNetworkMonitor *monitor;
+  gboolean network_available;
+
+  gtk_init (&argc, &argv);
+ 
+  monitor = g_network_monitor_get_default ();
+  network_available = g_network_monitor_get_network_available (monitor);
+
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+
+  gtk_widget_set_size_request(window,400,400);
+
+  frame = gtk_fixed_new();
+  gtk_container_add(GTK_CONTAINER(window), frame);
+  
+  button = gtk_button_new_with_label("Add Account");
+  gtk_widget_set_size_request(button, 100, 35);
+  gtk_fixed_put(GTK_FIXED(frame), button, 150, 150);
+
+  if(network_available){
+  	gtk_widget_set_sensitive(button,TRUE);}
+  else{ gtk_widget_set_sensitive(button,FALSE);}
+
+  gtk_widget_show_all (window);
+  
+  gtk_main ();
+
+  return 0;
+}
+
+
