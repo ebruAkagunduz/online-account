5bd54096fe848ec1642d5308695e693479b580be
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-11-28 07:29:30 +0200

    i added a function that can be used when network status changed

--- addAccount.c
+++ addAccount.c
@@ -1,21 +1,31 @@
 #include <gtk/gtk.h>
 #include <gio/gio.h>
 
-int
-main (int   argc,
-char *argv[])
-{
+GtkWidget *button;
+GNetworkMonitor *monitor;
+gboolean network_available;
+
+void increase(GNetworkMonitor *monitor,gboolean available,gpointer user_data){
+	
+	if(netw_status()){
+        	gtk_widget_set_sensitive(button,TRUE);}
+	else{ gtk_widget_set_sensitive(button,FALSE);}
+	}
+
+int netw_status(){
+	monitor = g_network_monitor_get_default ();
+	network_available = g_network_monitor_get_network_available (monitor);
+	return network_available;
+}
+
+
+int main (int argc,char *argv[]){
+
   GtkWidget *window;
-  GtkWidget *button;
   GtkWidget *frame;
-  GNetworkMonitor *monitor;
-  gboolean network_available;
 
   gtk_init (&argc, &argv);
  
-  monitor = g_network_monitor_get_default ();
-  network_available = g_network_monitor_get_network_available (monitor);
-
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
@@ -28,10 +38,13 @@ char *argv[])
   button = gtk_button_new_with_label("Add Account");
   gtk_widget_set_size_request(button, 100, 35);
   gtk_fixed_put(GTK_FIXED(frame), button, 150, 150);
+ 
+  if(netw_status()){
+	  gtk_widget_set_sensitive(button,TRUE);} 
+  else{
+	  gtk_widget_set_sensitive(button,FALSE);}
 
-  if(network_available){
-  	gtk_widget_set_sensitive(button,TRUE);}
-  else{ gtk_widget_set_sensitive(button,FALSE);}
+  g_signal_connect(monitor,"network-changed",G_CALLBACK(increase),window);
 
   gtk_widget_show_all (window);
   
